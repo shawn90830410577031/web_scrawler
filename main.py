79ca1efa756c459b6e4a12cf04265d0ddf35a6f9
shawn90830410577031 <33082378+shawn90830410577031@users.noreply.github.com>
2021-06-04 15:53:14 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows.
+
+# Python program to demonstrate
+# selenium
+
+import requests
+from bs4 import BeautifulSoup
